8236e0f45f096c93014c1dedc7f71b8b4bcbdc39
dong67226722 <huafa.dong@groupfu.com>
2020-06-14 11:47:41 +0800

    add continue

--- product.py
+++ product.py
@@ -2,11 +2,13 @@
 products = []
 with open ("products.csv", "r", encoding = "utf-8") as f:
 	for line in f:
+		if "商品,价格" in line:
+			continue
 		name, price = line.strip().split(",")
 		products.append([name, price])
 		print(products)
 
-
+#输入新商品
 while True:
 	name = input("请输入商品名称：")
 	if name == "q":
@@ -15,11 +17,12 @@ while True:
 	products.append([name, price])
 print(products)
 
+#输出资料
 for p in products:
 	print(p[0], "的价格是", p[1])
 
 
-
+#写入档案
 with open ("products.csv", "w", encoding = "utf-8") as f:
 	f.write("商品,价格\n")
 	for p in products:
